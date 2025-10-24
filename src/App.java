cb1750cbd612cb1cfc8a66609ab418345836eb32
M. Paz Jimenez <meripeich@gmail.com>
2025-10-24 13:08:33 +0200

    añadiendo los cambios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ejercicio en pareja de Pull-Request!");
+
+        System.out.println("Añadiendo cambios");
     }
 }
